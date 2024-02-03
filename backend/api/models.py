311908f2de28c5234b488f8010c829fec5a2baea
baArlene <carloschinyuku@gmail.com>
2024-02-03 21:16:12 +0200

    Create models

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.db.models.signals import post_save
 
 # Create your models here.
+
+class User(AbstractUser):
+    username = models.CharField(max_length=100)
+    email= models.EmailField(unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def profile(self):
+        profile = Profile.objects.get(user=self)
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=255)
+    bio = models.CharField(max_length=1000)
+    is_verified = models.BooleanField(default=False)        
+
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
