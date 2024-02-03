a987273b56fbdf59759b26fe4a954a0483503950
baArlene <carloschinyuku@gmail.com>
2024-02-03 23:47:32 +0200

    updating views,urls  & serializers

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework import generics
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework import status
+
+from .models import *
+from .serializers import *
 
 # Create your views here.
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTOPS
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny, )
+    serializer_class = RegistrationSerializer
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def protectedView(request):
+    output = f'Welcome {request.user}, Authentication Succeful' 
+    return Response({"response": output}, status=status.HTTP_200_OK)       
+
+@api_view(["GET"])
+def view_all_routes(request):
+    data = [
+        'api/token/refresh/',
+        'api/register/',
+        'api/token/'
+    ]
+
+    return Response(data)
