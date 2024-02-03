d127e841aa3057af9b476a073f3a4b031da5c21b
baArlene <carloschinyuku@gmail.com>
2024-02-03 21:45:33 +0200

    +++

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from models import *
+from .models import *
 
 # Register your models here.
 
@@ -9,7 +9,7 @@ class UserAdmin(admin.ModelAdmin):
 
 
 class ProfileAdmin(admin.ModelAdmin):
-    list_editable=['is_vberified']
+    list_editable=['is_verified']
     list_display= ['full_name', 'user', 'is_verified']
 
 admin.site.register(User, UserAdmin)
