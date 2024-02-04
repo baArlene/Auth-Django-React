3f944accd36e37c39eab1f5c3e1c434f3bb12d94
baArlene <carloschinyuku@gmail.com>
2024-02-04 22:15:45 +0200

    ++

---
+++ frontend/src/context/AuthContext.tsx
@@ -0,0 +1,15 @@
+import { jwtDecode } from "jwt-decode"
+import { createContext, useEffect, useState } from "react"
+import { useNavigate } from "react-router-dom"
+
+const swal = require('sweetalert2')
+
+
+
+const AuthContext = () => {
+  return (
+    <div>AuthContext</div>
+  )
+}
+
+export default AuthContext
